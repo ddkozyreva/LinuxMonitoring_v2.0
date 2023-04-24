aabfb4ee5b963f62506d15da167e6a2c099c286c
Loreta Themis <loretath@oa-d5.msk.21-school.ru>
2023-04-24 14:53:29 +0300

    03

--- src/03/main.sh
+++ src/03/main.sh
@@ -39,43 +39,31 @@ main_process() {
 
     if [ $choice -eq 2 ];
     then
-        regex_data_formet="202[23]-(0\d|1[0-2])-(0[1-9]|[12]\d|3[01]) ([01]\d|2[0-3]):([0-5]\d)"
-        elements_for_deletion=$(find $location -newermt "$start_time" ! -newermt "$end_time")
-        # rm -rf $elements_for_deletion
-        for file in $elements_for_deletion;
-        do
-            echo $file
-        done
-
-
-        #  if ! [[ "$(date "+%Y-%m-%d %H:%M" -d "$Date_Finish $Time_Finish" 2> /dev/null)" = "$Date_Finish $Time_Finish" ]]
-        #     then
-        # #     if  [[ "$Date_Finish"='(0?[1-9]|[12][0-9]|3[01])'-'(0?[1-9]|1[012])'-'[2][2-3]' ]] && [[ "$Time_Finish"='([1][1-9]|[2][0-3])':'([1-5][0-9]|[0-9])' ]]    
-        #         echo ""
-        #         echo "You entered an invalid search end date, example: (YYYY-MM-DD HH:MM)"
-        #         read -p "Enter the end time in the following format (YYYY-MM-DD HH:MM): " Date_Finish Time_Finish   
-        #     else
-        #         data=0     
+        regex_data_format="202[23]-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]) ([01][0-9]|2[0-3]):([0-5][0-9])"
+        if [[ $start_time =~ $regex_data_format ]] && [[ $end_time =~ $regex_data_format ]];
+        then
+            elements_for_deletion=$(find $location -newermt "$start_time" ! -newermt "$end_time")
+            for file in $elements_for_deletion;
+            do
+                echo $file
+                # rm -rf $file #$elements_for_deletion
+            done
+        else
+            echo "Incorrect data/time format. Please, try again in format 'YYYY-MM-DD HH:MM 'YYYY-MM-DD HH:MM'"
+        fi    
 
     fi
 
     if [ $choice -eq 3 ];
     then
-
-
-        # elements_for_deletion=$(find $location -newermt "$start_time" ! -newermt "$end_time")
-        # rm -rf $elements_for_deletion
-       # 4 и более символов - буквы A-Za-z, далее _, затем дата корректного формата:
-        regex_folder_name='[A-Za-z]{4,}_(0[1-9]|[12]\d|3[01])(0[1-9]|1[0-2])2[23]'
+        # 4 и более символов - буквы A-Za-z, далее _, затем дата корректного формата:
+        regex_folder_name='[A-Za-z]{4,}_(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[0-2])2[23]'
         data_for_deletion=$(find $location -type d | grep -E $regex_folder_name)
         for element_for_deletion in $data_for_deletion;
-        do echo $element_for_deletion
+        do 
+        echo $element_for_deletion
+        # rm -rf $element_for_deletion 
         done
-
-        #../02/access_log
-        # echo $(find / -type d 2>/dev/null | grep -E $regex_folder_name) ../02/log.txt
-
-        # \.\/(s+w+e+_160423\/)+a+s+_160423\.tr # https://regexr.com/
     fi
 }
 
